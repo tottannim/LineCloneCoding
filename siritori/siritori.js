0282014dc54b9b279399bcf1ba7284571cc9b073
tottannim <66168476+tottannim@users.noreply.github.com>
2020-09-16 20:48:19 +0900

    かたかな、ひらがなの変換必要

---
+++ siritori/siritori.js
@@ -0,0 +1,11 @@
+var word = 'らいむ';
+
+while (true){
+    //    合ってる時
+    var newWord = prompt(word);
+    if(word[word.length - 1] === newWord[0]){
+        word = newWord;
+    } else {
+    // 　　合ってない    
+    }
+}
